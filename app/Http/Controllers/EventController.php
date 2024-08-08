ac0d8ea7518fa787908af5a44da7f1ebc1084407
lucasmargui <lucasmargui@outlook.com>
2024-08-08 16:34:30 -0300

    feat: add event as participant dashboard

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -99,7 +99,9 @@ class EventController extends Controller
         $user = auth()->user();
         $events = $user->events;
 
-        return view('events.dashboard', ['events' => $events]);
+        $eventsAsParticipant = $user->eventsAsParticipant;
+
+        return view('events.dashboard', ['events' => $events, 'eventsasparticipant' => $eventsAsParticipant]);
     }
 
     public function destroy($id){ 
@@ -112,7 +114,13 @@ class EventController extends Controller
 
     public function edit($id){
 
+        $user = auth()->user();
+
         $event = Event::findOrFail($id);
+
+        if($user->id != $event->user_id){
+            return redirect('/dashboard');
+        }
 
         return view('events.edit', ['event' => $event]);
      }
